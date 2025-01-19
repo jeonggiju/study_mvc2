ed3d7299619695aec9e8fefcc3d2b0589746c075
jeonggiju <halogiju123@gmail.com>
2025-01-19 17:50:47 +0900

    2025/01/18

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -11,44 +11,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
-import java.lang.reflect.InaccessibleObjectException;
 
+/**
+ * @RestController
+ * RestController를 사용하면 해당 컨트롤러에 모두 @ResponseBody가 적용된다.
+ * 즉, RestController 는 Controller, ResponseBody를 합친 것과 같은 역할을 한다.
+ */
 @Slf4j
 @Controller
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
-    public void responseBodyV1(HttpServletResponse response) throws IOException
-    {
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
         response.getWriter().write("ok");
     }
-    /**
-     * HttpEntity, ResponseEntity(Http Status 추가)
-     * @return
-     */
+
     @GetMapping("/response-body-string-v2")
-    public ResponseEntity<String> responseBodyV2() {
+    public ResponseEntity<String> responseBodyV2(){
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
 
     @ResponseBody
     @GetMapping("/response-body-string-v3")
-    public String responseBodyV3() {
+    public String responseBodyV3(){
         return "ok";
     }
 
     @GetMapping("/response-body-json-v1")
-    public ResponseEntity<HelloData> responseBodyJsonV1() {
+    public ResponseEntity<HelloData> responseJsonV1(){
         HelloData helloData = new HelloData();
-        helloData.setUsername("userA");
         helloData.setAge(20);
+        helloData.setUsername("userA");
         return new ResponseEntity<>(helloData, HttpStatus.OK);
     }
-    
-    @ResponseStatus(HttpStatus.OK)
+
+    // 실제 API를 만들 때는 아래와 같은 스타일을 쓴다.
     @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/response-body-json-v2")
-    public HelloData responseBodyJsonV2() {
+    public HelloData responseBodyJsonV2(){
         HelloData helloData = new HelloData();
         helloData.setUsername("userA");
         helloData.setAge(20);
