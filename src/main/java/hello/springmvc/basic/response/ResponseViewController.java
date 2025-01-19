1689283f65c0800c893cc8f4ed7bc1165a5332dc
jeonggiju <halogiju123@gmail.com>
2025-01-19 16:02:52 +0900

    2025/01/19

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.response;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,7 +12,19 @@ public class ResponseViewController {
     @RequestMapping("/response-view-v1")
     public ModelAndView responseViewV1() {
         ModelAndView mav = new ModelAndView("response/hello").addObject("data","test");
-
         return mav;
+    }
+
+//    @ResponseBody // response/hello 가 뷰로 넘어가지 않고 고대로 출력됨.
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data","test");
+        return "response/hello";
+    }
+
+    // 강사는 권장하지 않는 방법
+    @RequestMapping("/response/hello") // <- 요게 view 이름으로 진행된다.
+    public void responseViewV3(Model model) {
+        model.addAttribute("data","test");
     }
 }
